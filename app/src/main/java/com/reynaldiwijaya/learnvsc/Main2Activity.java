733c0e63d0596356fd57cd6147fabd27bea11b4a
Reynaldi <reynaldiwijaya2306@gmail.com>
2019-04-11 15:01:07 +0700

    Test Commit and Push

---
+++ app/src/main/java/com/reynaldiwijaya/learnvsc/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.reynaldiwijaya.learnvsc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    //Example
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
